da7b275982fd3132dc6d51a06521f7238ce82514
shiguangwei <shigw5420@joyowo.com>
2019-03-20 23:29:20 +0800

    参数改为 对象

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
 myButton.addEventListener('click', () => {
-  jQuery.ajax('http://localhost:8082/xxx', 'POST', 'a=1&b=2', (res)=>{
-    console.log(res)
-  },(e)=>{
-    console.log(e)
+  jQuery.ajax({
+    url: 'http://localhost:8082/xxx',
+    method: 'POST',
+    data: 'a=1&b=2',
+    successFn: res => {
+      console.log(res)
+    },
+    errorFn: () => {}
   })
 })
 
-window.jQuery = function (nodeOrSelector) {
+window.jQuery = function(nodeOrSelector) {
   var nodes = {}
   nodes.xxx = function() {}
   return nodes
 }
 
-window .jQuery.ajax = function(url, method, data, successFn, errorFn) {
+window.jQuery.ajax = function(options) {
+  let url = ''
+  if (arguments.length === 1) {
+    url = options.url
+  } else {
+    url = arguments[0]
+    options = arguments[1]
+  }
+  let method = options.method
+  let data = options.data
+  let successFn = options.successFn
+  let errorFn = options.errorFn
+
   var xhr = new XMLHttpRequest()
   xhr.open(method, url, true)
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
       console.log(`status is ${xhr.status}`)
       if (xhr.status >= 200 && xhr.status < 400) {
         successFn.call(undefined, xhr.responseText)
-      }else {
+      } else {
         errorFn.call(undefined, xhr)
       }
     }
   }
 
-  xhr.send()
+  xhr.send(data)
 }
