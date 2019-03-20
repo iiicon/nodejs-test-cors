f1f614a9863755c51050fae016b5965379b846a4
shiguangwei <shigw5420@joyowo.com>
2019-03-21 00:06:39 +0800

    改写为jquery的promise

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 myButton.addEventListener('click', () => {
-  jQuery.ajax({
+  var result = jQuery.ajax({
     url: 'http://localhost:8082/xxx',
     method: 'POST',
-    data: 'a=1&b=2',
-    successFn: res => {
-      console.log(res)
-    },
-    errorFn: () => {}
+    data: 'a=1&b=2'
+  }).then((res)=>{
+    console.log(res)
+    console.log(result)
+  },(e)=>{
+    console.log(e)
   })
 })
 
@@ -16,31 +17,20 @@ window.jQuery = function(nodeOrSelector) {
   return nodes
 }
 
-window.jQuery.ajax = function(options) {
-  let url = ''
-  if (arguments.length === 1) {
-    url = options.url
-  } else {
-    url = arguments[0]
-    options = arguments[1]
-  }
-  let method = options.method
-  let data = options.data
-  let successFn = options.successFn
-  let errorFn = options.errorFn
-
-  var xhr = new XMLHttpRequest()
-  xhr.open(method, url, true)
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      console.log(`status is ${xhr.status}`)
-      if (xhr.status >= 200 && xhr.status < 400) {
-        successFn.call(undefined, xhr.responseText)
-      } else {
-        errorFn.call(undefined, xhr)
+window.jQuery.ajax = function({ url, method, data }) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest()
+    xhr.open(method, url, true)
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        console.log(`status is ${xhr.status}`)
+        if (xhr.status >= 200 && xhr.status < 400) {
+          resolve.call(undefined, xhr.responseText)
+        } else {
+          reject.call(undefined, xhr)
+        }
       }
     }
-  }
-
-  xhr.send(data)
+    xhr.send(data)
+  })
 }
